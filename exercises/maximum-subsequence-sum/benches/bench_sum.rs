10160129fc03f565da998d4428e99e121dbd1fef
Paul Hansen <paul.c.hansen@gmail.com>
2025-11-30 23:16:36 -0800

    finally decent benchmark comparison of my maximum subsequence sum algorithms over up to 10000 inputs

--- exercises/maximum-subsequence-sum/benches/bench_sum.rs
+++ exercises/maximum-subsequence-sum/benches/bench_sum.rs
@@ -1,12 +1,44 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{
+    criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion, PlotConfiguration,
+};
 use maximum_subsequence_sum::{
-    max_sub_sum_cubic, max_sub_sum_linear, max_sub_sum_nlogn, max_sub_sum_quadratic,
+    generate_random_integers, max_sub_sum_cubic, max_sub_sum_linear, max_sub_sum_nlogn,
+    max_sub_sum_quadratic,
 };
 use std::hint::black_box;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let r = [1, 2, 3, -100, 4, 5];
-    c.bench_function("blah", |b| b.iter(|| max_sub_sum_cubic(&r)));
+    // let r = [1, 2, 3, -100, 4, 5];
+
+    // Single group to produce comparison plots in the report
+    let mut group = c.benchmark_group("comparison");
+    group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
+
+    for &size in [10usize, 100, 1000, 10000].iter() {
+        group.throughput(criterion::Throughput::Elements(size as u64));
+
+        let r = generate_random_integers(size, -10, 10);
+
+        // Benchmark each algorithm with a distinct BenchmarkId; Criterion will
+        // place them together in the group's charts so you can compare growth.
+        group.bench_with_input(BenchmarkId::new("cubic", size), &r, |b, r| {
+            b.iter(|| max_sub_sum_cubic(black_box(r)));
+        });
+
+        group.bench_with_input(BenchmarkId::new("quadratic", size), &r, |b, r| {
+            b.iter(|| max_sub_sum_quadratic(black_box(r)));
+        });
+
+        group.bench_with_input(BenchmarkId::new("nlogn", size), &r, |b, r| {
+            b.iter(|| max_sub_sum_nlogn(black_box(r)));
+        });
+
+        group.bench_with_input(BenchmarkId::new("linear", size), &r, |b, r| {
+            b.iter(|| max_sub_sum_linear(black_box(r)));
+        });
+    }
+
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
