9769383beae8dd4fbd087dc88be906acd076765e
Paul Hansen <paul.c.hansen@gmail.com>
2025-11-30 22:04:34 -0800

    WIP trying to use criterion for benchmarking, but it doesn't compile yet

---
+++ exercises/maximum-subsequence-sum/benches/bench_sum.rs
@@ -0,0 +1,11 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
+use maximum_subsequence_sum::{max_sum_cubic, max_sum_quadratic, max_sum_nlogn, max_sum_linear};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let r = [1, 2, 3, -100, 4, 5];
+    c.bench_function("blah", |b| b.iter(|| max_sum_cubic(&r)))
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
